65199974df529206a290ea31565b3d469f6e6b26
JayRenegade <jayrenegade@hotmail.com>
2015-05-26 15:44:19 +0200

    Initial commit.

---
+++ TaskSandbox/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSandbox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Task task = new Task(() => {
+
+				Console.WriteLine("Begin of main task.");
+
+				Task subTask1 = new Task(() => {
+					Console.WriteLine("About to piss away 1000 milliseconds.");
+					System.Threading.Thread.Sleep(1000);
+					Console.WriteLine("Pissed away 1000 milliseconds.");
+				}, TaskCreationOptions.AttachedToParent);
+
+				subTask1.Start();
+
+				Task subTask2 = new Task(() => {
+					Console.WriteLine("About to piss away 2000 milliseconds.");
+					System.Threading.Thread.Sleep(2000);
+					Console.WriteLine("Pissed away 2000 milliseconds.");
+				}, TaskCreationOptions.AttachedToParent);
+
+				subTask2.Start();
+
+				Console.WriteLine("End of main task.");
+			});
+
+			task.Start();
+
+			task.Wait();
+
+			Console.WriteLine("ReadKey()");
+
+			Console.ReadKey();
+		}
+	}
+}
